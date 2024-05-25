8603a0852550ba7737fc8742ed728ef5584d7cf2
m.sredzinski94 <m.sredzinski94@gmail.com>
2024-05-25 14:27:31 +0200

    Created repositories abstraction

---
+++ BusinessLibrary/Repositories/ICustomersRepository.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLibrary.Models;
+
+namespace BusinessLibrary.Repositories;
+
+public interface ICustomersRepository
+{
+    Task<CustomerDto> CreateCustomerAsync(CustomerDto customer);
+    Task DeleteCustomerAsync(int id);
+    Task<CustomerDto> GetCustomerByIdAsync(int id);
+    Task<IEnumerable<CustomerDto>> GetCustomersAsync();
+    Task<CustomerDto> UpdateCustomerAsync(CustomerDto customer);
+}
---
+++ BusinessLibrary/Repositories/IOrdersRepository.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLibrary.Models;
+
+namespace BusinessLibrary.Repositories;
+
+public interface IOrdersRepository
+{
+    Task<OrderDto> CreateOrderAsync(OrderDto order);
+    Task DeleteOrderAsync(int id);
+    Task<OrderDto> GetOrderByIdAsync(int id);
+    Task<IEnumerable<OrderDto>> GetOrdersAsync();
+    Task<OrderDto> UpdateOrderAsync(OrderDto order);
+}
