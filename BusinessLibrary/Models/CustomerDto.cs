ce71c3bf97c5b06e2399d3ba52415fcb38e036df
m.sredzinski94 <m.sredzinski94@gmail.com>
2024-05-25 14:27:16 +0200

    Added DTO models

---
+++ BusinessLibrary/Models/CustomerDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessLibrary.Models;
+
+public class CustomerDto
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public IEnumerable<OrderDto> Orders { get; set; } = [];
+}
---
+++ BusinessLibrary/Models/OrderDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessLibrary.Models;
+
+public class OrderDto
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string? ProductName { get; set; }
+    public decimal Price { get; set; }
+}
