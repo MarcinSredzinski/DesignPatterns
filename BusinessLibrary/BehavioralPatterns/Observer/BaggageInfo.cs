62d414aafb339802b0261b06960b63e8341400b2
m.sredzinski94 <m.sredzinski94@gmail.com>
2024-06-17 23:22:00 +0200

    Added baggage info class for Observer pattern example

---
+++ BusinessLibrary/BehavioralPatterns/Observer/BaggageInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLibrary.BehavioralPatterns.Observer;
+
+public readonly struct BaggageInfo(int FlightNumber, string From, int Carousel);
