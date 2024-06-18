c700afaed2566809fba21345b3c047adc40629eb
m.sredzinski94 <m.sredzinski94@gmail.com>
2024-06-18 22:32:30 +0200

    Added usage examples for adapter pattern

---
+++ BusinessLibrary/StructuralPatterns/Adapter/ClientClassOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLibrary.StructuralPatterns.Adapter
+{
+    internal class ClientClassOne
+    {
+        private IClientInterface _clientInterface;
+        internal ClientClassOne(IClientInterface clientInterface)
+        {
+            _clientInterface = clientInterface;
+        }
+
+        /// <summary>
+        /// Here some client specific logic can happen. The client doesn't know have to know anything about the legacy class, 
+        /// nor is it forced to set it up. 
+        /// </summary>
+        internal void DoSomeClientOperation()
+        {
+            _clientInterface.Add(1, 2);
+        }
+    }
+}
---
+++ BusinessLibrary/StructuralPatterns/Adapter/ClientClassTwo.cs
@@ -0,0 +1,21 @@
+﻿namespace BusinessLibrary.StructuralPatterns.Adapter
+{
+    internal class ClientClassTwo
+    {
+        private IClientInterface _clientInterface;
+        internal ClientClassTwo(IClientInterface clientInterface)
+        {
+            _clientInterface = clientInterface;
+        }
+
+        /// <summary>
+        /// Here some client specific logic can happen. The client doesn't know have to know anything about the legacy class, 
+        /// nor is it forced to set it up. This can only need a part of the logic provided by the adapter, but then it's worth considering 
+        /// interface segregation principle. 
+        /// </summary>
+        internal void DoSomeClientOperation()
+        {
+            _clientInterface.Substract(1, 2);
+        }
+    }
+}
